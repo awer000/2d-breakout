c25b4e79d257f6e0e1fa2906814afd57d13e07c6
awer000 <awer000@naver.com>
2021-06-05 14:59:14 +0900

    feat: 패들 키보드로 컨트롤 하기

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,14 @@ let dy = -2;
 
 const ballRadius = 10;
 
+// paddle 정보
+const paddleHeight = 10;
+const paddleWidth = 75;
+let paddleX = (canvas.width - paddleWidth) / 2;
+
+let rightPressed = false;
+let leftPressed = false;
+
 function drawBall() {
     ctx.beginPath();
     ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
@@ -16,9 +24,18 @@ function drawBall() {
     ctx.closePath();
 }
 
+function drawPaddle() {
+    ctx.beginPath();
+    ctx.rect(paddleX, canvas.height - paddleHeight, paddleWidth, paddleHeight);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    ctx.closePath();
+}
+
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     drawBall();
+    drawPaddle();
 
     if (y + dy < ballRadius || y + dy > canvas.height - ballRadius) {
         dy = -dy
@@ -30,7 +47,34 @@ function draw() {
 
     x += dx;
     y += dy;
+
+    if(rightPressed && paddleX < canvas.width-paddleWidth) {
+        paddleX += 7;
+    }
+    else if(leftPressed && paddleX > 0) {
+        paddleX -= 7;
+    }
     requestAnimationFrame(draw)
 }
 
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
+
+function keyDownHandler(e: KeyboardEvent) {
+    if(e.keyCode == 39) {
+        rightPressed = true;
+    }
+    else if(e.keyCode == 37) {
+        leftPressed = true;
+    }
+}
+
+function keyUpHandler(e: KeyboardEvent) {
+    if(e.keyCode == 39) {
+        rightPressed = false;
+    }
+    else if(e.keyCode == 37) {
+        leftPressed = false;
+    }
+}
 requestAnimationFrame(draw)
