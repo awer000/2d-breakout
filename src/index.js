bc5a292b2a8b3f8375b67873962805d5ef912dd3
awer000 <awer000@naver.com>
2021-06-05 14:50:32 +0900

    feat: 공을 벽에 튕기기

--- src/index.js
+++ src/index.js
@@ -4,9 +4,10 @@ var x = canvas.width / 2;
 var y = canvas.height - 30;
 var dx = 2;
 var dy = -2;
+var ballRadius = 10;
 function drawBall() {
     ctx.beginPath();
-    ctx.arc(x, y, 10, 0, Math.PI * 2);
+    ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
     ctx.fillStyle = "#0095DD";
     ctx.fill();
     ctx.closePath();
@@ -14,6 +15,14 @@ function drawBall() {
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     drawBall();
+    if (y + dy < ballRadius || y + dy > canvas.height - ballRadius) {
+        dy = -dy;
+    }
+    ;
+    if (x + dx < ballRadius || x + dx > canvas.width - ballRadius) {
+        dx = -dx;
+    }
+    ;
     x += dx;
     y += dy;
     requestAnimationFrame(draw);
--- src/index.ts
+++ src/index.ts
@@ -6,9 +6,11 @@ let y = canvas.height - 30;
 let dx = 2;
 let dy = -2;
 
+const ballRadius = 10;
+
 function drawBall() {
     ctx.beginPath();
-    ctx.arc(x, y, 10, 0, Math.PI * 2);
+    ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
     ctx.fillStyle = "#0095DD";
     ctx.fill();
     ctx.closePath();
@@ -17,6 +19,15 @@ function drawBall() {
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     drawBall();
+
+    if (y + dy < ballRadius || y + dy > canvas.height - ballRadius) {
+        dy = -dy
+    };
+
+    if (x + dx < ballRadius || x + dx > canvas.width - ballRadius) {
+        dx = -dx;
+    };
+
     x += dx;
     y += dy;
     requestAnimationFrame(draw)
